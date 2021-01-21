05b03ae0998dab7dc5249a6a3d1d08e1bfd05e32
Sn34k3rS <erwann.guillemin@icloud.com>
2021-01-21 19:32:16 +0100

    deploy on heroku

--- server.js
+++ server.js
@@ -12,17 +12,6 @@ const Stripe = require('stripe')(key.stripeSecret);
 app.use(cors());
 app.use(bodyParser());
 
-// Production Part
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, 'client/build')));
-    app.get('*', (req, res)=> {
-        res.sendFile(path.join(__dirname, 'client/build', 'index.html'));
-    })
-}
-
-
-app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
-
 // Routes
 app.post("/pay", async (req, res)=> {
     const {amount, paymentMethod, receipt_email} = req.body;
@@ -43,3 +32,13 @@ app.post("/pay", async (req, res)=> {
         });
     }
 });
+
+// Production Part
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static('client/build'));
+    app.get('*', (req, res)=> {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
+
+app.listen(PORT, () => {console.log(`Server is running on port ${PORT}`)});
