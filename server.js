407d6671ceb2765842d8c183ee0d6686302d0021
Sn34k3rS <erwann.guillemin@icloud.com>
2021-01-21 19:11:06 +0100

    new test

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const bodyParser = require("body-parser");
 const app = express();
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
 const path = require('path');
 
 const key = require('./config/dev')
