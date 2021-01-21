489d8d5da8fd4e334d81e0b0ec6da407bc7df55b
Sn34k3rS <erwann.guillemin@icloud.com>
2021-01-21 20:13:31 +0100

    add something

--- server.js
+++ server.js
@@ -35,9 +35,9 @@ app.post("/pay", async (req, res)=> {
 
 // Production Part
 if (process.env.NODE_ENV === 'production') {
-    app.use(express.static('client/build'));
+    app.use(express.static(path.join(__dirname, "client/build")));
     app.get('*', (req, res)=> {
-        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+        res.sendFile(path.join(__dirname+'/client/build/index.html'));
     });
 }
 
